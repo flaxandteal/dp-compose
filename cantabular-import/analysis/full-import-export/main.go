ff745b673d0b10d22ee1dea5cfeb8ed42f26b0d6
red <red54321@outlook.com>
2022-02-07 12:10:07 +0000

    Update import for new cantabular fields

--- cantabular-import/analysis/full-import-export/main.go
+++ cantabular-import/analysis/full-import-export/main.go
@@ -658,14 +658,14 @@ func postCreateUniqueRecipe(token string) (string, error) {
 	uri := recipeAPIHost + "/recipes"
 	fmt.Printf("uri: %s\n", uri)
 
-	body := fmt.Sprintf(`{"alias": "%s","format": "cantabular_table","id": "%s","cantabular_blob": "Example","output_instances": [{"code_lists": [{"href": "http://localhost:22400/code-lists/city-regions","id": "city","is_hierarchy": false,"name": "City"},{"href": "http://localhost:22400/code-lists/siblings_3","id": "siblings_3","is_hierarchy": false,"name": "Number Of Siblings (3 mappings)"},{"href": "http://localhost:22400/code-lists/sex","id": "sex","is_hierarchy": false,"name": "Sex"}],"dataset_id": "%s","editions": ["2021"],"title": "%s"}]}`, alias, uuid, datasetIdName, title)
+	body := fmt.Sprintf(`{"alias": "%s","format": "cantabular_table","id": "%s","cantabular_blob": "Example","output_instances": [{"code_lists": [{"href": "http://localhost:22400/code-lists/city-regions","id": "city","is_hierarchy": false,"is_cantabular_geography": false,"name": "City"},{"href": "http://localhost:22400/code-lists/siblings_3","id": "siblings_3","is_hierarchy": false,"is_cantabular_geography": false,"name": "Number Of Siblings (3 mappings)"},{"href": "http://localhost:22400/code-lists/sex","id": "sex","is_hierarchy": false,"is_cantabular_geography": false,"name": "Sex"}],"dataset_id": "%s","editions": ["2021"],"title": "%s"}]}`, alias, uuid, datasetIdName, title)
 
 	// This is Fran's recipie as of 20.12.2021 for generating more than 1M rows, for Census V12
 	// !!! BUT need to set up recipie, etc in mongo for it to play ball (and there may be some cantabular database stuff as well)
 	/*datasetIdName := "Testing-xlsx-census-v12" + "-" + uuid
 	alias := "Testing xlsx census v12"
 	title := "Testing xlsx census v12"
-	body := fmt.Sprintf(`{"alias": "%s","cantabular_blob": "People-Households","format": "cantabular_table","id": "%s","output_instances": [{"code_lists": [{ "href": "http://localhost:22400/code-lists/lsoa","id": "lsoa","is_hierarchy": false,"name": "Lower Super Output Area"},{ "href": "http://localhost:22400/code-lists/age", "id": "age","is_hierarchy": false,"name": "age"}],"dataset_id": "%s","editions": ["2021"],"title": "%s"}]}`, alias, uuid, datasetIdName, title)*/
+	body := fmt.Sprintf(`{"alias": "%s","cantabular_blob": "People-Households","format": "cantabular_table","id": "%s","output_instances": [{"code_lists": [{ "href": "http://localhost:22400/code-lists/lsoa","id": "lsoa","is_hierarchy": false,"is_cantabular_geography": false,"name": "Lower Super Output Area"},{ "href": "http://localhost:22400/code-lists/age", "id": "age","is_hierarchy": false,"is_cantabular_geography": false,"name": "age"}],"dataset_id": "%s","editions": ["2021"],"title": "%s"}]}`, alias, uuid, datasetIdName, title)*/
 
 	fmt.Printf("Body: %s\n", body)
 
