f6eb1737bf1175aa9377350737c59e4c3ea7e84e
Mark Ryan <mryan321@gmail.com>
2022-08-08 09:12:54 +0100

    Update with resources for e2e interactives journey

--- v2/provisioning/ons/zebedee/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if test -d "../zebedee"
-  then
-      # it wll exit 1 but the important bit is it builds the jar file :)
-      docker-compose --env-file=start-default.env run --entrypoint "/The-Train/run.sh && exit" the-train
-fi
